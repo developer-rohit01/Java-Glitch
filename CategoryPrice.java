77c306f97a6d6b82c36be4880bd7c4b90c236c8d
Rohit <rr5450534@gmail.com>
2026-01-17 19:54:36 +0530

    Add the Taking input from user

--- CategoryPrice.java
+++ CategoryPrice.java
@@ -28,3 +28,22 @@ class Product {
 
         System.out.print("Enter number of products: ");
         int n = sc.nextInt();
+
+         // Taking dynamic input
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter details of product " + (i + 1));
+
+            System.out.print("ID: ");
+            int id = sc.nextInt();
+
+            System.out.print("Name: ");
+            String name = sc.next();
+
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+
+            System.out.print("Category: ");
+            String category = sc.next();
+
+            products.add(new Product(id, name, price, category));
+        }
