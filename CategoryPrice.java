3f3149c9cc1429d0ad5318f9625652084deb76bd
Rohit <rr5450534@gmail.com>
2026-01-17 19:54:00 +0530

    Add the main class taking input

--- CategoryPrice.java
+++ CategoryPrice.java
@@ -5,3 +5,26 @@ class Product {
     private String name;
     private double price;
     private String category;
+
+     // Constructor
+    public Product(int id, String name, double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+
+
+
+
+
+
+    public class ProductDemo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Product> products = new ArrayList<>();
+
+        System.out.print("Enter number of products: ");
+        int n = sc.nextInt();
