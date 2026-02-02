875813b5c6b7f231e0ca95aa742c3b30faa4df36
Rohit <rr5450534@gmail.com>
2026-02-02 22:05:34 +0530

    Add the Do while code initiate

---
+++ DoWhilw.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DoWhileDemo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        do {
+            System.out.print(i + " ");
+            i++;
+        } while (i <= n);
+
+        sc.close();
+    }
+}
