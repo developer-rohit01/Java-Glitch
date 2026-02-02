ee0a6595a2c323a01d444896917e849b90d2d976
Rohit <rr5450534@gmail.com>
2026-02-02 22:15:08 +0530

    Add count the digit code

---
+++ CountDigits.java
@@ -0,0 +1,15 @@
+public class CountDigits {
+
+    public static void main(String[] args) {
+
+        int num = 50720;
+        int count = 0;
+
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+
+        System.out.println(count);
+    }
+}
