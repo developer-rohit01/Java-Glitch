e783182c0dcb2356fe46f48c68d88794534a3fdf
Rohit <rr5450534@gmail.com>
2026-02-04 21:53:49 +0530

    Add the code to call Method

---
+++ main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Student {
+    String name;
+    int age;
+
+    void display() {
+        System.out.println("Name = "+name + "\nAge = " + age);
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Student s1 = new Student(); // object creation
+        System.out.print("Enter name: ");
+        s1.name = sc.nextLine();
+        System.out.print("Enter age: ");
+        s1.age = sc.nextInt();
+
+        s1.display();// method call\
+        sc.close();
+    }
+}
