7b626804d0e860b867db7205674d6e34099c8da4
Rohit <rr5450534@gmail.com>
2026-02-23 21:57:55 +0530

    Add character to be input

---
+++ Chara.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+public class Chara {
+  public static void main(String[] args) {
+    Scanner sc =  new Scanner(System.in);
+
+    System.out.print("Enter the A: ");
+    int a=sc.nextInt();
+    System.out.print("Enter the B: ");
+    char b=sc.next().charAt(4);
+    
+    System.out.println("The A is: "+a);
+    System.out.println("The B is: "+b);   
+
+  }
+}
