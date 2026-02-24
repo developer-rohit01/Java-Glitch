d5f7a78aaa73ad57f85499c25ebc50b603b94184
Rohit <rr5450534@gmail.com>
2026-02-24 19:15:35 +0530

    Update chara

--- Chara.java
+++ Chara.java
@@ -1,16 +1,15 @@
+
 import java.util.Scanner; 
 
 public class Chara {
   public static void main(String[] args) {
     Scanner sc =  new Scanner(System.in);
-
-    System.out.print("Enter the A: ");
-    int a=sc.nextInt();
-    System.out.print("Enter the B: ");
-    char b=sc.next().charAt(4);
-    
-    System.out.println("The A is: "+a);
-    System.out.println("The B is: "+b);   
-
+      System.out.print("Enter the A: ");
+      int a=sc.nextInt();
+      System.out.print("Enter the B: ");
+      char b=sc.next().charAt(1);
+      
+      System.out.println("The A is: "+a);
+      System.out.println("The B is: "+b);
+    }
   }
-}
