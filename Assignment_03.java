c62fba13dd5553fb2feebb3f7cb302aed18cccdd
Rohit <rr5450534@gmail.com>
2026-03-10 18:21:34 +0530

    Add Assignment_03

---
+++ Assignment_03.java
@@ -0,0 +1,46 @@
+public class Assignment_03 {
+
+  public static void main(String[] args) {
+
+        // 1. String Creatio
+        String s1 = "Hello";
+        String s2 = new String("Hello");
+
+        System.out.println("String 1: " + s1);
+        System.out.println("String 2: " + s2);
+
+        // 2. String Comparison
+
+        // Using equals()
+        System.out.println("\nUsing equals(): " + s1.equals(s2));
+
+        // Using ==
+        System.out.println("Using == : " + (s1 == s2));
+
+        // Using compareTo()
+        System.out.println("Using compareTo(): " + s1.compareTo(s2));
+
+        // 3. String Immutability
+        String str = "Java";
+        str.concat(" Programming");
+
+        System.out.println("\nAfter concat without assignment: " + str);
+
+        str = str.concat(" Programming");
+
+        System.out.println("After concat with assignment: " + str);
+
+        // 4. String Methods
+
+        String text = "Java Programming";
+
+        System.out.println("\nLength: " + text.length());
+        System.out.println("Uppercase: " + text.toUpperCase());
+        System.out.println("Lowercase: " + text.toLowerCase());
+        System.out.println("Character at index 2: " + text.charAt(2));
+        System.out.println("Substring (5,16): " + text.substring(5,16));
+        System.out.println("Contains 'Java': " + text.contains("Java"));
+        System.out.println("Replace 'Java' with 'Python': " + text.replace("Java","Python"));
+    }
+  
+}
