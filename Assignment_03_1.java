bd14daa276649bc55a390bfe64db81581a88e763
Rohit <rr5450534@gmail.com>
2026-04-05 22:10:57 +0530

    Add Assignment_03_1 Wrapper Class code

---
+++ Assignment_03_1.java
@@ -0,0 +1,22 @@
+//wrapper class for Assignment 03, question 1
+
+public class Assignment_03_1 {
+  public static void main(String[] args) {
+    // premitive data types
+    int a = 10;
+    double b = 20.5;
+
+    // wrapper classes
+    Integer aWrapper = Integer.valueOf(a);
+    Double bWrapper = Double.valueOf(b);
+
+    System.out.println("Primitive int: " + a);
+    System.out.println("Wrapper Integer: " + aWrapper);
+    System.out.println("Primitive double: " + b);
+    System.out.println("Wrapper Double: " + bWrapper);
+
+    
+
+  }
+  
+}
