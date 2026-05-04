dfa2ba5694489cd2d10b6062517c859150a87a31
Rohit <rr5450534@gmail.com>
2026-05-04 15:05:39 +0530

    Add Exam solution code string constant pool

---
+++ gy.java
@@ -0,0 +1,12 @@
+public class gy {
+    public static void main(String[] args) {
+        String name = "Rohit";
+        String face ="Rohit";
+        String gase= new String("Rohit");  
+        System.out.println(name==face);
+        System.out.println(name==gase);
+        System.out.println(face.equals(gase));
+        
+    }
+  
+}
